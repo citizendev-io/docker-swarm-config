95685f0d6e6513b157e482d09cb5f4c56732f587
Tran Cong Viet An <trancongvietan22@gmail.com>
2023-05-06 08:24:19 +0000

    fix: traefik configuration

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+curl https://get.docker.com | bash
+usermod -aG docker ubuntu
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s $(pwd)/deploy-docker-swarm.sh /usr/local/bin/deploy-docker-swarm
